f7554e16ab4d9cfdc7a63263685e0e2d1554f787
Debbie <debz_scott@icloud.com>
2024-03-30 23:20:30 +0000

    tidied up display

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const container = document.querySelector(".container")
-
+const container = document.querySelector(".container");
+const smallbtn = document.querySelector(".small");
 const multiple = 16 * 16;
-console.log(multiple)
+
+smallbtn.addEventListener("click", () => {
+    
+    
+
+})
 
 for(let i=0; i<multiple; i++) {
     const createDiv = document.createElement("div");
@@ -9,6 +14,34 @@ for(let i=0; i<multiple; i++) {
     createDiv.style.height ="100px";
     createDiv.style.background ="pink";
     createDiv.style.border = "1px solid black";
+    createDiv.className = "squares"
     
 container.appendChild(createDiv);
+
+
+};
+
+const squares = document.querySelector(".squares")
+
+squares.onmousedown = () => {
+
+    squares.style.background = "black";
 }
+
+
+
+/*squares.forEach(div => div.addEventListener("mousedown", () => {
+
+squares.style.background = "black";
+
+    
+}));
+
+
+
+
+
+squares.onmousedown = () => {
+    
+    squares.style.background = "black";
+}*/
